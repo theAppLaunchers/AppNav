bec92c766bf67223243860b5abd5f90c7dc7f9b9
Mark Evans <mark.vans@icloud.com>
2022-06-09 17:36:01 -0600

    Initial Commit

---
+++ AppNav/AppNavApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppNavApp.swift
+//  AppNav
+//
+//  Created by Mark Evans on 6/9/22.
+//
+
+import SwiftUI
+
+@main
+struct AppNavApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
